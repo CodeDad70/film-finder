31ef4554b0689d0be5a20927b3875d4ddf6962fb
CodeDad <github email address>
2018-08-16 01:32:59 -0400

    returned AllFavorites link to a stateless component

--- film-finder-client/src/components/AllFavorites.js
+++ film-finder-client/src/components/AllFavorites.js
@@ -1,46 +1,15 @@
-import React, {Component} from 'react' ;
-import {connect} from 'react-redux';
-import {getFavorites} from '../actions/favorites'
-import { Redirect } from 'react-router'
-
-class AllFavorites extends Component {
-  constructor() {
-    super()
-    this.state = {
-      
-      fireRedirectFavorites: false,
-    }
-  } 
+import React from 'react';
+import { Link } from 'react-router-dom';
+ 
+const AllFavorites = () => {
   
-
-  handleClick = () => {  
-    this.props.getFavorites()
-    this.setState({ fireRedirectFavorites: true })
-  }
-
-
-  render() {
-    const { fireRedirectFavorites } = this.state
-     
-    return (
-      <div>
-       <button onClick={this.handleClick}>Your WatchList</button>
-       {fireRedirectFavorites && <Redirect to={`/watchlist`} />}
-      </div>
-    )
-    }
-  }
-
-  
-
-  const mapStateToProps = (state) => {
-    return ({
-     
-      getFavorites: state.getFavorites,
-
-    })
-  }
-  
-  export default connect(mapStateToProps, {
-    getFavorites,
-  })(AllFavorites)
+ 
+  return (
+    <div>
+       <Link to={'/watchlist'}>Your WatchList</Link>
+    </div>
+  );
+};
+ 
+export default AllFavorites;
+ 
