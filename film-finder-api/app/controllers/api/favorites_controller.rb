6f30c4777a2e2a7556f67fb21823b48d2a1dbe75
CodeDad <github email address>
2018-08-15 22:31:05 -0400

    created favorites controller with whitelist

---
+++ film-finder-api/app/controllers/api/favorites_controller.rb
@@ -0,0 +1,50 @@
+class Api::FavoritesController < ApplicationController
+
+  def index 
+    render json: Favorite.all
+  end
+
+  def show 
+    render json: Favorite.find_by(id: params[:id])
+  end
+
+  def create
+    favorite = Favorite.new(favorite_params)
+    if favorite.save
+      render json: favorite
+    else
+      render json: {message: favorite.errors}, status: 400 
+    end
+  end  
+
+  def update 
+    if @favorite.update(favorite_params)
+      render json: @favorite
+    else
+      render json: {message: @favorite.errors}, status: 400
+    end
+  end
+
+  def destroy
+    if @favorite.destroy
+      render status: 204
+    else
+      render json: {message: "Unable to delete"}, status: 400
+    end
+  end
+  
+  private
+
+    def set_favorite
+      @favorite = Favorite.find_by(id: params[:id])
+    end
+
+    def favorite_params
+      params.require(:favorite).permit(:title, :backdrop_path, :moviedb_id, :overview, :popularity, :post_path, :release_date)
+    end  
+
+
+  end
+
+end
+
